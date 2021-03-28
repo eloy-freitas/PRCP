fd8f5d64b31f6c74ea1ee5fe2a90cae03b0d1258
eloy-freitas <44293812+eloy-freitas@users.noreply.github.com>
2021-03-28 18:00:11 -0300

    Delete src directory

--- src/header/solucao.h
+++
@@ -1,6 +0,0 @@
-#include "../solucao.c"
-
-Solucao *criarLista();
-int inserirSolucao(Solucao *lista, Solucao *solucao);
-Solucao *criarSolucao(int id, int fo, int pl, int nc, int L, int C);
-int imprimirSolucoes(Solucao* solucao);
